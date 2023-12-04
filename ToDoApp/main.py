4f92394c21cc84d1a2b798f7642c592211de66f7
syedhaideralizaidi <haider.zaidiy@gmail.com>
2023-12-04 17:10:28 +0500

    DB added

---
+++ ToDoApp/main.py
@@ -0,0 +1,36 @@
+from typing import Annotated
+from fastapi import FastAPI, Depends, HTTPException, Path
+from sqlalchemy.orm import Session
+from starlette import status
+
+import models
+from models import Todos
+from database import engine, SessionLocal
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def read_all(db: db_dependency):
+    return db.query(Todos).all()
+
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail="Todo not found.")
